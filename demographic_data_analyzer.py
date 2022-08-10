e4fe248bb083a76e656c80bd3b43ca38fd1bb6c7
Bankook0115 <lmlc11218@gmail.com>
2022-08-10 20:51:29 +0700

    Revise csv file path

--- demographic_data_analyzer.py
+++ demographic_data_analyzer.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 def calculate_demographic_data(print_data=True):
     # Read data from file
-    df = pd.read_csv('boilerplate-demographic-data-analyzer-main/adult.data.csv')
+    df = pd.read_csv('adult.data.csv')
 
     # How many of each race are represented in this dataset? This should be a Pandas series with race names as the index labels.
     race_count = df['race'].value_counts()
